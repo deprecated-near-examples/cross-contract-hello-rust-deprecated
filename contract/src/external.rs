eaa337af2ce9bd86128d8e351e6aa0f5b5fd889e
gagdiez <gagdiez.c@gmail.com>
2022-11-17 11:12:33 +0100

    removed unnecesary trait

--- contract/src/external.rs
+++ contract/src/external.rs
@@ -4,13 +4,6 @@ pub const TGAS: u64 = 1_000_000_000_000;
 pub const NO_DEPOSIT: u128 = 0;
 pub const XCC_SUCCESS: u64 = 1;
 
-// Interface of this contract, for callbacks
-#[ext_contract(this_contract)]
-trait Callbacks {
-  fn query_greeting_callback(&mut self) -> String;
-  fn change_greeting_callback(&mut self) -> bool;
-}
-
 // Validator interface, for cross-contract calls
 #[ext_contract(hello_near)]
 trait HelloNear {
